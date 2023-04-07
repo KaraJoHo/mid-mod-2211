a666804916c867c66c39a66f4e4938e3af3e5dd4
KaraJH <karas.jones@yahoo.com>
2023-04-07 10:02:05 -0600

    feat: Faraday connection/api call and creating food poro

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,18 @@
 class FoodsController < ApplicationController 
   def index 
-    
+    # require 'pry'; binding.pry
+    conn = Faraday.new(url: "https://api.nal.usda.gov/fdc/v1/" ) do |faraday|
+      faraday.headers["X-Api-Key"] = ENV["FOOD_KEY"]
+    end
+
+    search_response = conn.get("foods/search?query=#{params[:q]}")
+
+    parsed = JSON.parse(search_response.body, symbolize_names: true)
+   
+
+    @count = parsed[:totalHits]
+    @foods = parsed[:foods].map do |food_info| 
+      Food.new(food_info)
+    end[0..9]
   end
 end

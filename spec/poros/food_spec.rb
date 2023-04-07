3eda66b5803ebeed84bea11dc9f65d039f8bc6a7
KaraJH <karas.jones@yahoo.com>
2023-04-07 10:01:05 -0600

    test: Food class

--- spec/poros/food_spec.rb
+++ spec/poros/food_spec.rb
@@ -1,3 +1,32 @@
 require "rails_helper"
 
-RSpec.describe
+RSpec.describe Food do 
+  before(:each) do 
+    attrs = {
+              :fdcId=>983525,
+              :description=>"SWEET POTATOES",
+              :dataType=>"Branded",
+              :gtinUpc=>"8901020020844",
+              :publishedDate=>"2020-06-26",
+              :brandOwner=>"NOT A BRANDED ITEM",
+              :ingredients=>"ORGANIC SWEET POTATOES.",
+              :marketCountry=>"United States",
+              :foodCategory=>"Pre-Packaged Fruit & Vegetables",
+              :modifiedDate=>"2020-04-13",
+              :dataSource=>"LI",
+              :servingSizeUnit=>"g",
+              :servingSize=>133.0
+    }
+    @food = Food.new(attrs)
+  end
+
+  describe "Food" do 
+    it "exists and has attributes" do 
+      expect(@food).to be_a(Food)
+      expect(@food.description).to eq("SWEET POTATOES")
+      expect(@food.gtinupc).to eq("8901020020844")
+      expect(@food.brand_owner).to eq("NOT A BRANDED ITEM")
+      expect(@food.ingredients).to eq("ORGANIC SWEET POTATOES.")
+    end
+  end
+end
